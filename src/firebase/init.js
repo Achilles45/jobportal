6d5df44106c85f1288e7ba0813f5e24604a66ae1
Achilles45 <achillesusuoyibo7@gmail.com>
2020-06-15 16:03:31 -0700

    Finished the project

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -2,15 +2,15 @@ import firebase from 'firebase';
 import firestore from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDpMKds6Oan01O90BHouwPbN_p6Cqy1t1U",
-    authDomain: "job-portal-3ee65.firebaseapp.com",
-    databaseURL: "https://job-portal-3ee65.firebaseio.com",
-    projectId: "job-portal-3ee65",
-    storageBucket: "job-portal-3ee65.appspot.com",
-    messagingSenderId: "461981130073",
-    appId: "1:461981130073:web:75387071ceb94c0d3d0048",
-    measurementId: "G-CN7FPBHQLM"
-  };
+  apiKey: "AIzaSyBgVhtVXFaeyqqVoYzXSF4L_anUCia6lTQ",
+  authDomain: "job-portal-a58a4.firebaseapp.com",
+  databaseURL: "https://job-portal-a58a4.firebaseio.com",
+  projectId: "job-portal-a58a4",
+  storageBucket: "job-portal-a58a4.appspot.com",
+  messagingSenderId: "845073328824",
+  appId: "1:845073328824:web:383ebc731e4c690f1c624d",
+  measurementId: "G-1YZZ53B0LZ"
+};
   // Initialize Firebase
   const firebaseApp = firebase.initializeApp(firebaseConfig);
   export default firebaseApp.firestore();
